50a91083bded6d122f947a7b69405efcf893ab39
moljac <mcvjetko@holisticware.net>
2013-04-19 08:54:29 +0200

    Patterns for file exclusion added

--- ProjectLinker/Utility/FilterManager.cs
+++ ProjectLinker/Utility/FilterManager.cs
@@ -22,44 +22,73 @@ using Microsoft.Practices.ProjectLinker;
 
 namespace Microsoft.Practices.ProjectLinker.Utility
 {
-    public static class FilterManager
-    {
-        private const string ProjectLinkerExcludeFilterKey = "ProjectLinkerExcludeFilter";
-        private const char ExpressionsSeparator = ';';
-        private static readonly string[] DefaultFilterExpressions = new[] { @"\\?desktop(\\.*)?$", @"\\?silverlight(\\.*)?$", @"\.desktop", @"\.silverlight", @"\.xaml", @"^service references(\\.*)?$", @"\.clientconfig", @"^web references(\\.*)?$" };
+	public static class FilterManager
+	{
+		private const string ProjectLinkerExcludeFilterKey = "ProjectLinkerExcludeFilter";
+		private const char ExpressionsSeparator = ';';
+		private static readonly string[] DefaultFilterExpressions = 
+			new[] 
+			{ 
+			  @"\\?desktop(\\.*)?$"
+			, @"\\?silverlight(\\.*)?$"
+			, @"\.desktop"
+			, @"\.silverlight"
+			, @"\.xaml"
+			, @"\.WP.cs"				// exclude Windows Phone specific code
+			, @"\.MA.cs"				// exclude Mono For Android specific code
+			, @"\.MT.cs"				// exclude MonoTouch specific code
+			, @"\.WF.cs"				// exclude Windows Forms specific code
+			, @"\.WPF.cs"				// exclude WPF specific code
+			, @"\.ASPNET.cs"			// exclude ASP.net specific code
+			, @"\.SLRIA.cs"				// exclude Silverlight (RIA-Browser) specific code
+			, @"\.MM.cs"				// exclude Mono Mobile (Mono common) specific code
+			, @"\.Mono.cs"				// exclude Mono specific code
+			, @"^service references(\\.*)?$"
+			, @"\.clientconfig"
+			, @"^web references(\\.*)?$" 
+			, @"\\?\.WP(\\.*)?$"		// Folders with extension WP
+			, @"\\?\.MA(\\.*)?$"		// Folders with extension MA
+			, @"\\?\.MT(\\.*)?$"		// Folders with extension MT
+			, @"\\?\.WF(\\.*)?$"		// Folders with extension WF
+			, @"\\?\.WPF(\\.*)?$"		// Folders with extension WPF
+			, @"\\?\.ASPNET(\\.*)?$"	// Folders with extension ASPNET
+			, @"\\?\.SLRIA(\\.*)?$"		// Folders with extension SLRIA
+			, @"\\?\.MM(\\.*)?$"		// Folders with extension MM
+			, @"\\?\.Mono(\\.*)?$"		// Folders with extension Mono
+			};
 
-        public static IProjectItemsFilter GetFilterForProject(Project project)
-        {
-            if (project == null)
-                throw new ArgumentNullException("project");
+		public static IProjectItemsFilter GetFilterForProject(Project project)
+		{
+			if (project == null)
+				throw new ArgumentNullException("project");
 
-            if (!project.Globals.get_VariableExists(ProjectLinkerExcludeFilterKey))
-            {
-                SetFilterForProject(project, DefaultFilterExpressions);
-            }
-            string excludeFilterExpressions = (string) project.Globals[ProjectLinkerExcludeFilterKey];
+			if (!project.Globals.get_VariableExists(ProjectLinkerExcludeFilterKey))
+			{
+				SetFilterForProject(project, DefaultFilterExpressions);
+			}
+			string excludeFilterExpressions = (string) project.Globals[ProjectLinkerExcludeFilterKey];
 
-            IProjectItemsFilter filter = new RegexProjectItemsFilter(excludeFilterExpressions.Split(new[] {';'},
-                                                                                                    StringSplitOptions.
-                                                                                                        RemoveEmptyEntries));
+			IProjectItemsFilter filter = new RegexProjectItemsFilter(excludeFilterExpressions.Split(new[] {';'},
+																									StringSplitOptions.
+																										RemoveEmptyEntries));
 
-            return filter;
-        }
+			return filter;
+		}
 
-        public static void SetFilterForProject(Project project, IEnumerable<string> filterExpressions)
-        {
-            StringBuilder sb = new StringBuilder();
-            foreach (string filterExpression in filterExpressions)
-            {
-                if (sb.Length > 0)
-                {
-                    sb.Append(ExpressionsSeparator);
-                }
-                sb.Append(filterExpression);   
-            }
+		public static void SetFilterForProject(Project project, IEnumerable<string> filterExpressions)
+		{
+			StringBuilder sb = new StringBuilder();
+			foreach (string filterExpression in filterExpressions)
+			{
+				if (sb.Length > 0)
+				{
+					sb.Append(ExpressionsSeparator);
+				}
+				sb.Append(filterExpression);   
+			}
 
-            project.Globals[ProjectLinkerExcludeFilterKey] = sb.ToString();
-            project.Globals.set_VariablePersists(ProjectLinkerExcludeFilterKey, true);
-        }
-    }
+			project.Globals[ProjectLinkerExcludeFilterKey] = sb.ToString();
+			project.Globals.set_VariablePersists(ProjectLinkerExcludeFilterKey, true);
+		}
+	}
 }
--- README.md
+++ README.md
@@ -1,3 +1,62 @@
+#ProjectLinker tool for Visual Studio 2010 by HolisticWare team
+
+This is modification of the original Patterns & Practices Project Linker tool
+for HolisticWare Project.
+
++ []() 
++ []() 
++ []() 
++ []() 
+
+## DONE
+1	Exclusion file patterns for cross platform development  
+	WP - Windows Phone
+	MT - MonoTouch  
+	MA - Mono for Android  
+	WF - Windows Forms
+	WPF - WPF    
+	SLRIA - Silverlight RIA   
+	ASPNET - ASP.net   
+	MM - Mono Mobile   
+	Mono - Mono general  
+	1	Exclude patterns in csproj file excluding 
+		1	source files (*.cs)  
+			\.WP.cs  
+			\.MA.cs  
+			\.MT.cs   
+			\.SLRIA.cs  
+			\.WF.cs  
+			\.WPF.cs  
+			\.ASPNET.cs  
+			\.MM.cs  
+			\.Mono.cs  
+		2	directories
+			\\?\.WP(\\.*)?$  
+			\\?\.MA(\\.*)?$  
+			\\?\.MT(\\.*)?$  
+			\\?\.SLRIA(\\.*)?$  
+			\\?\.WF(\\.*)?$  
+			\\?\.WPF(\\.*)?$  
+			\\?\.ASPNET(\\.*)?$  
+			\\?\.MM(\\.*)?$  
+			\\?\.Mono(\\.*)?$  
+
+			
+Paste following strings at the begginging of ProjectLinkerExcludeFilter attribute
+	\.WP.cs;\.MA.cs;\.MT.cs;\.SLRIA.cs;\.WF.cs;\.WPF.cs;\.ASPNET.cs;\.MM.cs;\.Mono.cs;\\?\.WP(\\.*)?$;\\?\.MA(\\.*)?$;\\?\.MT(\\.*)?$;\\?\.SLRIA(\\.*)?$;\\?\.WF(\\.*)?$;\\?\.WPF(\\.*)?$;\\?\.ASPNET(\\.*)?$;\\?\.MM(\\.*)?$;\\?\.Mono(\\.*)?$;
+
+  <ProjectExtensions>
+    <VisualStudio>
+		<UserProperties 
+			ProjectLinkerExcludeFilter="\\?desktop(\\.*)?$;\\?silverlight(\\.*)?$;\.desktop;\.silverlight;\.xaml;^service references(\\.*)?$;\.clientconfig;^web references(\\.*)?$" 
+			ProjectLinkReference="Some guid" 
+		/>
+    </VisualStudio>
+  </ProjectExtensions>
+
+## TODOs
+1	Fix exception during addink source project (
+
 #ProjectLinker tool for Visual Studio 2010
 
 This is the original Patterns & Practices Project Linker tool updated for today's developer tools.
